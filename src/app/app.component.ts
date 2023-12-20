dff14932c2e48dbd94181803d9d66f055294bb49
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-12-20 12:41:09 +0100

    added function and interpolaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculator_angular';
+
+  calculatorValue: number = 0;
+  funcText: any = 'NoFunction';
+
+  onClickValue(val: string, type: any) {
+    console.log(val, type);
+  }
 }
